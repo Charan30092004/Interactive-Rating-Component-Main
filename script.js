96618bfe7e2b9e824382dafb84c19f7701f6bb66
Charan30092004 <121110924+Charan30092004@users.noreply.github.com>
2023-03-18 02:51:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
 function myFunction()
 {
     result=document.getElementById("score");
-    let selected = document.querySelector('input[type="radio"]:checked').value;
-    console.log(selected);
-    result.innerHTML=selected;
+    result.innerHTML=z;
     document.getElementById("firstpage").style.display="none";
     document.getElementById("finalpage").style.display="block";
+}
+function onover()
+{
+    document.getElementById("button").style.color="hsl(25, 97%, 53%)";
+    document.getElementById("button").style.backgroundColor="hsl(0, 0%, 100%)";
+    document.getElementById("button").style.borderColor="hsl(0, 0%, 100%)";
+}
+function original()
+{
+    document.getElementById("button").style.color="hsl(0, 0%, 100%)";
+    document.getElementById("button").style.backgroundColor="hsl(25, 97%, 53%)";
+    document.getElementById("button").style.borderColor="hsl(25, 97%, 53%)";
+}
+let ids=document.getElementsByClassName("credit");
+function over(x)
+{
+    if(z!=x)
+    {
+    x=x-1;
+    test=0;
+    ids[x].style.backgroundColor="hsl(25, 97%, 53%)";
+    }
+}
+let test=0;
+function leave (y) 
+{
+    if(test==0  && z!=y)
+    {
+    y=y-1;
+    for (let i=0;i<5;i++)
+    {
+        if (i != y){
+                ids[y].style.backgroundColor="hsl(220, 22%, 24%)";
+        }
+    }
+    }
+}
+var z;
+function score(z)
+{
+    test=1;
+    window['z']=z;
+    z=z-1;
+    for(i=0;i<5;i++)
+    {
+        if(z==i)
+        {
+            ids[z].style.backgroundColor="rgba(207, 212, 218, 0.60)";
+        }
+        else
+        {
+            ids[i].style.backgroundColor="hsl(220, 22%, 24%)";
+        }
+    }
 }
